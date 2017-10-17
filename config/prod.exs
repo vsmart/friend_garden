3b30f4ccb97ab16c5dcdce25c7c91baef89c3682
Franziska Schmidt <hello@franka.tech>
2017-10-17 21:21:58 +0200

    copy paste :facepalm:

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :friend_garden, FriendGarden.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello, Hello.Repo,
+config :friend_garden, FriendGarden.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
